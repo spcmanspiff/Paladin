a8497a7b6d84fb28d56a14a4f2b8406a66e01f2a
Brad Scott <bradscott@apple.com>
2020-09-29 10:51:31 -0700

    Initial Commit

---
+++ Paladin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Paladin
+//
+//  Created by Brad Scott on 9/29/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
